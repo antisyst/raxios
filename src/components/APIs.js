75923aa6156701515b6a7eab6cb0a5ccd1fbb58d
Pratham82 <mali.prathamesh82@gmail.com>
2020-10-08 01:23:05 +0530

    Modified

--- src/components/APIs.js
+++ src/components/APIs.js
@@ -1,5 +1,1 @@
-export const featured_api =
-  "  https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cea6d01e6b0df1690e899d80de6a1958&page=1";
-export const search_api =
-  "https://api.themoviedb.org/3/search/movie?api_key=cea6d01e6b0df1690e899d80de6a1958&query=";
 export const img_api = "https://image.tmdb.org/t/p/w1280";
